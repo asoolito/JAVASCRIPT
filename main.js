83c5e46b0ed60cabc21abbea93c299364c0ea78c
asoolito <roblox890king890@gmail.com>
2024-05-04 17:15:38 +0500

    JAVASCRIPT

---
+++ main.js
@@ -0,0 +1,55 @@
+console.log("Javascript");
+console.log("Abduraxmon");
+
+var ism = " Abduraxmon";
+console.log(ism);
+var yosh=17;
+console.log(yosh);
+
+// var malumot = alert();
+
+// let familiya = "Axrorov"
+// console.log(familiya);
+
+// const boyim = 1.76;
+// console.log(boyim);
+
+// var ism="Jamhsid";
+
+// var ism ="Sevinch";
+
+// console.log(ism);
+
+// let ism="Sunnatbek";
+// ism="Javohir";
+// let ism ="Madina";
+
+// const ism ="Sunnatbek";
+// ism="Javohir";
+// const ism ="Sardor";
+
+// user-family> Kebab case
+// let user-family = "Sunnatbek";
+
+// let user_name = "Sunnatbek" snake case
+
+// let userName = "Sunnatbek"; camel case
+
+// let userAge = 17
+
+// let userSurname="Yusupov"
+// console.log(userSurname)
+
+// prompt("Ismingizni kiriting"); 
+let userName = prompt("Ismingizni kiriting");
+console.log(userName);
+let userSurname = prompt("familiyangizni iriting");
+console.log(userSurname);
+let userSubject= prompt("Yoqtirgan faningizni kiriting");
+console.log(userSubject);
+let userTuman= prompt("Yashaydigan tumaningizni kiriting");
+console.log(userTuman);
+let userAge = prompt("yoshingizin kiriting");
+console.log(userAge);
+let user = alert("ma'umotlaringiz uchun raxmat")
+console.log(user);
